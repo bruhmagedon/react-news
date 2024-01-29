91583e3f0d7f0c89ba39532888b6030418092d86
bruhmagedon <schubert.walk@yandex.ru>
2024-01-29 22:35:13 +0300

    add skeleton for news and banner

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {},
-    fontFamily: {
-      inter: ["Inter"],
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    theme: {
+        extend: {
+            // keyframes: {
+            //     pulse: {
+            //         "0%": { backgroundPosition: "0% 0%" },
+            //         "100%": { backgroundPosition: "-135% 0%" },
+            //     },
+            // },
+        },
+        fontFamily: {
+            inter: ["Inter"],
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 };
