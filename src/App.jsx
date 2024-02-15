5f025c239d75970728d0a3a2742b722cdf42fe4e
bruhmagedon <schubert.walk@yandex.ru>
2024-02-15 18:54:23 +0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Header from "./components/header/Header";
+import Header from "./components/header/Header.jsx";
 import Main from "./pages/Main/Main";
 
 function App() {
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,14 +2,6 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      // keyframes: {
-      //     pulse: {
-      //         "0%": { backgroundPosition: "0% 0%" },
-      //         "100%": { backgroundPosition: "-135% 0%" },
-      //     },
-      // },
-    },
     fontFamily: {
       inter: ["Inter"],
     },
