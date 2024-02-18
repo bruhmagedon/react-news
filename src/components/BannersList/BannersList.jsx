d9e26e2da51a73b407c96acc3c4769d1aacb8378
bruhmagedon <schubert.walk@yandex.ru>
2024-02-18 13:28:25 +0300

    end latestnews

--- src/components/BannersList/BannersList.jsx
+++ src/components/BannersList/BannersList.jsx
@@ -4,7 +4,7 @@ import NewsBanner from "../NewsBanner/NewsBanner";
 const BannersList = ({ banners }) => {
   return (
     <>
-      <ul className="banners-list w-full gap-[12px]">
+      <ul className="banners-list w-full gap-[12px] max-h-[1200px] overflow-y-auto">
         {banners?.map((banner) => {
           return <NewsBanner key={banner.id} item={banner} />;
         })}
--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,37 +1,11 @@
-import { PAGE_SIZE } from "../../constants/Constants";
-import { getNews } from "../../API/apiNews";
-import { useDebounce } from "../../helpers/hooks/useDebounce";
-import { useFetch } from "../../helpers/hooks/useFetch";
-import { useFilters } from "../../helpers/hooks/useFilters";
 import LatestNews from "../../components/LatestNews/LatestNews";
 import NewsByFilters from "../../components/NewsByFilters/NewsByFiltes";
 
 const Main = () => {
-  const { filters, changeFilter } = useFilters({
-    page_number: 1,
-    page_size: PAGE_SIZE,
-    category: null,
-    keywords: "",
-  });
-
-  const debouncedKeywords = useDebounce(filters.keywords, 1500);
-
-  const { data, isLoading } = useFetch(getNews, {
-    ...filters,
-    keywords: debouncedKeywords,
-  });
-
   return (
     <main className="main w-full gap-8 max-md:grid-cols-1">
-      {/* Список баннеров */}
-      <LatestNews isLoading={isLoading} banners={data && data.news} />
-      {/* Новости по фильтрам + поиск + пагинация */}
-      <NewsByFilters
-        news={data?.news}
-        isLoading={isLoading}
-        filters={filters}
-        changeFilter={changeFilter}
-      />
+      <LatestNews />
+      <NewsByFilters />
     </main>
   );
 };
