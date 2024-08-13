fea12cfee69bb95b12a14009839ce213dd60e69c
Maxim <schubert.walk@yandex.ru>
2024-08-13 18:19:23 +0300

    refactoring + fsd

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,26 @@
+/* eslint-disable no-undef */
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+module.exports = {
+  darkMode: ['selector'],
+  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
+  prefix: '',
   theme: {
-    fontFamily: {
-      inter: ["Inter"],
+    container: {
+      center: true,
+      padding: '2rem',
+      screens: {
+        '2xl': '1400px'
+      }
     },
+    extend: {
+      fontFamily: {
+        Segoe: ['Segoe UI', 'sans-serif']
+      },
+      colors: {
+        'dark-theme': '#202020',
+        'light-theme': '#f3f3f3'
+      }
+    }
   },
-  plugins: [],
+  plugins: [require('tailwindcss-animate')]
 };
